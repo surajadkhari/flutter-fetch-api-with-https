5c7995cee69cf14c3652b4ce667e47e60635869f
surajadkhari <surajadkhari@gmail.com>
2022-02-12 13:34:31 +0545

    first commit

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,37 @@
+import 'package:api_future/model/user_model.dart';
+import 'package:api_future/services/user_service.dart';
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('User Profile'),
+        ),
+        body: FutureBuilder<List<UserModel>>(
+          future: ApiService().getUser(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView(
+                children: [
+                  ...snapshot.data!.map((e) => ListTile(
+                        title: Text(e.firstname),
+                        subtitle: Text(e.lastname),
+                        
+                        trailing: CircleAvatar(
+                          backgroundImage: NetworkImage(e.avatar),
+                        ),
+                      ))
+                ],
+              );
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+            return const Center(child: CircularProgressIndicator());
+          },
+        ));
+  }
+}
